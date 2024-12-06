05d75f6ca860722867ee2b1ade9b303631ef3af8
Sakshi Kokadwar <sakshikokadwar03@gmail.com>
2024-12-06 14:33:13 +0530

    slideshow added successfully

--- script.js
+++ script.js
@@ -8,3 +8,30 @@
       const overlayElement = document.getElementById("overlay");
       overlayElement.style.display = 'none';
    }
+
+   const images = ["1.jpeg", "2.jpeg", "3.webp", "4.jpg", "5.jpg"];
+   
+   let index = 0;
+   const slide = document.getElementById("slide");
+
+      function next () {
+         if(index < images.length-1){
+             index++;
+         }
+         else{
+            index=0;
+         }
+         slide.src = images[index];
+   }
+
+   function prev() {
+      if(index > 0){
+         index--;
+     }
+     else{
+        index= images.length-1;
+     }
+     slide.src = images[index];
+   }
+
+   setInterval(next, 2000);
